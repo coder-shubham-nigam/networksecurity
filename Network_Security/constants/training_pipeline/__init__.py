9e7a4b7e74c15097d54bd8dd7eec3f092eb6b9ee
coder-shubham-nigam <shubham.nigam2005@gmail.com>
2026-09-11 15:03:26 +0530

    Data Validation completed

--- Network_Security/constants/training_pipeline/__init__.py
+++ Network_Security/constants/training_pipeline/__init__.py
@@ -24,3 +24,17 @@ ARTIFACT_DIR:str = "Artifacts"
 FILE_NAME:str = "phisingData.csv"
 TRAIN_FILE_NAME:str = "train.csv"
 TEST_FILE_NAME:str = "test.csv"
+
+
+""" 
+Data validation related constant start with Data_Validation VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
+DATA_VALIDATION_VALID_DIR:str = "validated"
+DATA_VALIDATION_INVALID_DIR:str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = "report.yaml"
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+SCHEMA_FILE_PATH = os.path.join(PROJECT_ROOT, "data_schema", "schema.yaml")
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Network_Security.components.data_ingestion import DataIngestion
+from Network_Security.components.data_validation import DataValidation
 from Network_Security.exception.exception import NetworkSecurityException
 from Network_Security.logging.logger import logging
-from Network_Security.entity.config_entity import DataIngestionConfig
+from Network_Security.entity.config_entity import DataIngestionConfig , DataValidationConfig
 from Network_Security.entity.config_entity import TrainingPipelineConfig
 import os
 import sys
@@ -13,6 +14,14 @@ if __name__ == "__main__":
         data_ingestion = DataIngestion(dataingestionconfig)
         logging.info("Inittated Data Ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation completed")
         print(dataingestionartifact)
+        data_validation_config = DataValidationConfig(trainingpipelineconfig)
+        datavalidation = DataValidation(dataingestionartifact , data_validation_config)
+        logging.info("Initiate the data validation")
+        data_validation_artifact = datavalidation.initiate_data_validation()
+        logging.info("Data validation completed")
+        print(data_validation_artifact)
+        
     except Exception as e:
         raise NetworkSecurityException(e,sys)
